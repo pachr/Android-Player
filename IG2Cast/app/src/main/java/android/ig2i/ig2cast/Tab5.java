e68095bd5446b1381af020f451fc982f7dd35071
kevin lawuy <kevin lawuy>
2016-05-18 10:03:11 +0200

    slider-menu version 1

---
+++ IG2Cast/app/src/main/java/android/ig2i/ig2cast/Tab5.java
@@ -0,0 +1,18 @@
+package android.ig2i.ig2cast;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Tab5 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.tab_5,container,false);
+        return v;
+    }
+}
